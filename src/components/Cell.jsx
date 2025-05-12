e2830c0c155339349c6f726296d675ebc632880b
grisha <grisha@MacBook-Pro-grisha.local>
2025-05-12 18:37:17 +0500

    add: add classnames, fix cell

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,28 +1,30 @@
 import Lottie from "lottie-react";
+import classNames from "classnames"
 import crossAnimation from "../assets/cross.json";
 import ovalAnimation from "../assets/oval.json";
 
-const Cell = ({ value, onClick, isWinning, isDraw, disabled }) => (
-  <button
-    className={`cell ${isWinning ? "winCombination" : ""} ${isDraw ? "draw-blink" : ""}`}
-    onClick={onClick}
-    disabled={disabled ?? !!value} 
-  >
-    {value === "X" && (
-      <Lottie
-        animationData={crossAnimation}
-        loop={false}
-        className="symbol-animation symbol-x"
-      />
-    )}
-    {value === "O" && (
-      <Lottie
-        animationData={ovalAnimation}
-        loop={false}
-        className="symbol-animation symbol-o"
-      />
-    )}
-  </button>
-);
+const Cell = ({ value, onClick, isWinning, isDraw, disabled }) => {
+
+  const cellClass = classNames("cell", {
+    blink: isWinning || isDraw,
+    winCombination: isWinning,
+    "draw-blink": isDraw && !isWinning,
+  });
+  const symbolClass = classNames("symbol-animation", {
+    "symbol-x": value === "X",
+    "symbol-o": value === "O",
+  });
+
+  return (
+    <button className={cellClass} onClick={onClick} disabled={disabled}>
+      {value && (
+        <div className={symbolClass}>
+          {value === "X" && <Lottie animationData={crossAnimation} loop={false} />}
+          {value === "O" && <Lottie animationData={ovalAnimation} loop={false} />}
+        </div>
+      )}
+    </button>
+  );
+}
 
 export default Cell;
